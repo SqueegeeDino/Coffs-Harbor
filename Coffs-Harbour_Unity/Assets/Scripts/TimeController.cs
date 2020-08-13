8ae30daea5323ccf41e33a563ecdbf85bca11180
SqueegeeDino <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-13 10:53:32 -0700

    1.0 Ready, working on day/night cycle
    1.0 Ready changes
    
    Basic day night cycle implemented
    Working on adding a moon and night light
    Need to have lightprobes update at runtime

---
+++ Coffs-Harbour_Unity/Assets/Scripts/TimeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    public Animation sunAnim;
+    public Slider TODSlider;
+    public GameObject[] nightLights;
+
+    private void Start()
+    {
+        Debug.Log("[Coffs-Harbor] Start");
+
+        // On start, initialize the animation, and freeze playback
+        sunAnim.Play("SunAnimation");
+        sunAnim["SunAnimation"].speed = 0;
+
+        // Disable all lights on start
+        foreach (GameObject i in nightLights)
+        {
+            i.SetActive(false);
+        }
+    }
+    public void Slider_Changed(float Slider_Value)
+    {
+        // Uncomment to debug the float of the slider value
+
+        // Debug.Log("[Coffs-Harbor] Slider Value" + Slider_Value);
+
+        // Sets the animation relative to the slider value
+        sunAnim["SunAnimation"].normalizedTime = Slider_Value;
+
+        // This all drives the lights turning on and off at certain times of day
+        if(Slider_Value <= .375f)
+        {
+            foreach (GameObject i in nightLights)
+            {
+                i.SetActive(false);
+            }
+        }
+        else
+        {
+            if(Slider_Value >= .7f)
+            {
+                foreach (GameObject i in nightLights)
+                {
+                    i.SetActive(false);
+                }
+            }
+            else
+            {
+                foreach (GameObject i in nightLights)
+                {
+                    i.SetActive(true);
+                }
+            }
+        }
+
+    }
+}
